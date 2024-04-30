5b28b54155b94bef32342faf8d2686ba848ed395
Mohitbasnet <mohitbasnet80@gmail.com>
2024-04-30 13:38:30 +0545

    hide backspace through pos

--- custom/mb_pos/static/src/js/mb_sample_button.js
+++ custom/mb_pos/static/src/js/mb_sample_button.js
@@ -4,8 +4,17 @@ odoo.define("mb_pos.MohitSampleButton", function(require) {
     const PosComponent = require("point_of_sale.PosComponent");
     const ProductScreen = require("point_of_sale.ProductScreen");
     const Registeries = require("point_of_sale.Registries")
+    const { useListener } = require("@web/core/utils/hooks");
     class MohitSampleButton extends PosComponent {
 
+        setup(){
+            super.setup();
+            useListener("click",this.mb_sample_button_click );
+        }
+
+        mb_sample_button_click(){
+            console.log("Hello this is button click event pressed")
+        }
 
 
     }
---
+++ custom/mb_pos_restrict/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mohitbasnet POS Restriction",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'license': 'LGPL-3',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','point_of_sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'assets':{
+        'point_of_sale.assets':[
+            "mb_pos_restrict/static/src/js/mb_sample_button.js",
+            "mb_pos_restrict/static/src/xml/mb_sample_button.xml"
+        ]
+    }
+}
