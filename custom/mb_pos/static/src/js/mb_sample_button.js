a5a03ae3137ec357f1f671d28fd5aa0b425d9826
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-01 11:10:12 +0545

    converted string into javascript

--- custom/mb_pos/static/src/js/mb_sample_button.js
+++ custom/mb_pos/static/src/js/mb_sample_button.js
@@ -5,6 +5,10 @@ odoo.define("mb_pos.MohitSampleButton", function(require) {
     const ProductScreen = require("point_of_sale.ProductScreen");
     const Registeries = require("point_of_sale.Registries")
     const { useListener } = require("@web/core/utils/hooks");
+    const  core  = require("web.core");
+    var _t = core._t;
+
+
     class MohitSampleButton extends PosComponent {
 
         setup(){
@@ -14,37 +18,37 @@ odoo.define("mb_pos.MohitSampleButton", function(require) {
 
         async mb_sample_button_click(){
             //  Sound pop up
-            // this.showPopup("ErrorPopup", {
-            //     title:"Error message",
-            //     body:"The simple Error message",
-            // });
+            this.showPopup("ErrorPopup", {
+                title: _t("Error message"),
+                body: this.env._t("The simple Error message"),
+            });
 
             // Confirm popup
-            // const { confirmed } = await this.showPopup("ConfirmPopup",{
-            //     title:"Confirm Popup",
-            //     body:"Are you sure to continue?",
-            //     confirmText: "Yes",
-            //     cancelText:"No"
-            // });
-            // if (confirm){
+            var { confirmed }= await this.showPopup("ConfirmPopup",{
+                title: _t("Confirm Popup"),
+                body: _t("Are you sure to continue?"),
+                confirmText: _t("Yes"),
+                cancelText: _t("No")
+            });
+            // if (confirmed){
             //     console.log("clicked to Yes button")
             // }else{
             //     console.log("clicked to No button")
             // }
             
             // offline error popup
-            // this.showPopup("OfflineErrorPopup",{
-            //     title:"Mohit Error",
-            //     body: "Hey this is the test popup screen okay don't take it serious!"
-            // });
+            this.showPopup("OfflineErrorPopup",{
+                title: _t("Mohit Error"),
+                body: this.env._t("Hey this is the test popup screen okay don't take it serious!")
+            });
 
             // Choice popup
-            // const { confirmed, payload: selectedOption } = await this.showPopup("SelectionPopup",{
-            //     title:"Are you a good python developer.",
-            // list:[{'id':0,'label':"Yes ",'item':"You pressed yes"},
-            //       {'id':1,'label':"No",'item':"You pressed No"},
-            //       {'id':2,'label':"In confusion",'item':"you pressed in confusion"}]
-            // });
+            var { confirmed, payload: selectedOption } = await this.showPopup("SelectionPopup",{
+                title: ("Are you a good python developer."),
+                list:[{'id':0,'label': _t("Yes "),'item':"You pressed yes"},
+                  {'id':1,'label': _t("No"),'item':"You pressed No"},
+                  {'id':2,'label': _t("In confusion"),'item':"you pressed in confusion"}]
+            });
 
             // console.log(confirmed);
             // console.log(selectedOption);
