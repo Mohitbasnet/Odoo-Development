b54519d7e6f916040fceb022b84e621ef39539aa
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-02 13:11:25 +0545

    added validation

--- custom/library_app/models/library_book.py
+++ custom/library_app/models/library_book.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 
-
+from odoo.exceptions import ValidationError
 class library_app(models.Model):
     _name = 'library.book'
     _description = 'Books'
@@ -20,6 +20,16 @@ class library_app(models.Model):
     author_ids = fields.Many2many("res.partner",
                                   srting="Authors")
     
+
+    @api.constrains('isbn')
+    def check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise ValidationError("Please provide an ISBN for %s" %book.name)
+            if book.isbn and not len(book.isbn) == 5:
+                raise ValidationError("%s ISBN is invalid\nNeeds to be five digits only" %book.isbn)
+            return True
+        
     # book_type = fields.Selection(
     #     [
     #         ("paper","Paperback"),
