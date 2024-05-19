fd5adc9ca4a7d070b53a942a0661a8d84a722994
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-19 16:08:44 +0545

    added security

---
+++ custom_addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Real Estate Ads",
+    "verison": "1.0",
+    "website": "https//www.mohitbasnet.com",
+    "author":"Mohit",
+    "description":"Nothing to say",
+    "licence":"LGPL-3",
+    "category":"Sales",
+    "application": True,
+    "installable": True,
+    "depend": [],
+    "data": [
+        "security/ir.model.access.csv"
+    ],
+}
---
+++ custom_addons/real_estate_ads/models/property.py
@@ -0,0 +1,24 @@
+from odoo import fields,models
+
+class Property(models.Model):
+    _name = 'estate.property'
+    _description = "This is the model made by mohit"
+    
+    name = fields.Char(string="Name")
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available from")
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(srting="Selling Price")
+    best_offer = fields.Float(string="Best Offer")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(String="Facades")
+    garage = fields.Boolean(string="Garage",default =False)
+    garden = fields.Boolean(string="Garden", default = False) 
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientaion = fields.Selection([
+        ('north','North'),('west',"West"),('east','East'),('south','South')
+        ],string="Garden Orientation",default='north')
+     
+
