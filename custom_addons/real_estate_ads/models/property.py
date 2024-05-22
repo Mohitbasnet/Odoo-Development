d7d7ed0cd21166c46a4ef442827b1db6a9bc3b4e
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-22 10:37:37 +0545

    added computed field

--- custom_addons/real_estate_ads/models/property.py
+++ custom_addons/real_estate_ads/models/property.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 class Property(models.Model):
     _name = 'estate.property'
@@ -26,7 +26,14 @@ class Property(models.Model):
 
     sales_id = fields.Many2one('res.users',string="Salesman")
     buyer_id = fields.Many2one('res.partner', string="Buyer")
-     
+    
+    
+    @api.depends("living_area","garden_area")
+    def _compute_total_area(self):
+        for rec in self:
+            rec.total_area = rec.living_area + rec.garden_area
+
+    total_area = fields.Integer(string="Total Area", compute="_compute_total_area")
 
 class PropertyType(models.Model):
     _name = 'estate.property.type'
--- custom_addons/real_estate_ads/models/property_offer.py
+++ custom_addons/real_estate_ads/models/property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models,api
+from datetime import timedelta
 
 
 class PropertyOffer(models.Model):
@@ -13,6 +14,20 @@ class PropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', string="Customer")
     property_id = fields.Many2one('estate.property', string="Property")
     validity = fields.Integer(string = "Validity")
-    deadline = fields.Date(string="Deadline")
+    deadline = fields.Date(string="Deadline", inverse = "_inverse_deadline")
+    creation_date = fields.Date(string="Create Date")
+
+
+    @api.depends("validity","creation_date")
+    def _compute_deadline(self):
+        for rec in self:
+            if rec.creation_date and rec.validity:
+                rec.deadline = rec.creation_date + timedelta(days=rec.validity)
+            else:
+                rec.deadline = False
+    
+    def _inverse_deadline(self):
+        for rec in self:
+            rec.validity = (rec.deadline - rec.creation_date).days
       
     
