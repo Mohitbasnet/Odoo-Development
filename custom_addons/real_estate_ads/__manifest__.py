0c29de0e9b74b44462df575e62b29bbed9afc8da
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-23 12:28:24 +0545

    added action and button

--- custom_addons/real_estate_ads/__manifest__.py
+++ custom_addons/real_estate_ads/__manifest__.py
@@ -1,22 +1,26 @@
 {
     "name": "Real Estate Ads",
-    "verison": "1.0",
-    "website": "https//www.mohitbasnet.com",
-    "author":"Mohit",
-    "description":"Nothing to say",
-    "licence":"LGPL-3",
-    "category":"Sales",
+    "version": "1.0",
+    "website": "https://www.mohitbasnet.com",
+    "author": "Mohit",
+    "description": "Nothing to say",
+    "license": "LGPL-3",
+    "category": "Sales",
     "application": True,
     "installable": True,
-    "depend": ['base'],
+    "depends": ['base'],
     "data": [
         "security/ir.model.access.csv",
-        "views/property_view.xml",
-        "views/menu_items.xml",
-        "views/property_tag.xml",
+         "views/menu_items.xml",
         "views/property_type.xml",
-    
-    # Data files
+        
+        "views/property_tag.xml",
+        "views/property_view.xml",
+        "views/property_offer_view.xml",
+       
+        
+        
+        
         # "data/property_type.xml",
         "data/estate.property.type.csv"
     ],
