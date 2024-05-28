3d8ed3eb6c9074113eba4bff82ba3739adf8af18
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-28 12:47:13 +0545

    added the view inheritance

---
+++ custom_addons/real_estate_ads_sales_person/models/res_users.py
@@ -0,0 +1,9 @@
+
+from odoo import models, fields, api
+
+
+class Users(models.Model):
+    _inherit = "res.users"
+
+    property_ids = fields.One2many("estate.property", "sales_id", string="Properties")
+
